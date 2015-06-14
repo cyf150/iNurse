9970aa2628c5ca6c89d031df4a3bc310dd017ae4
cyf150 <cyf150@qq.com>
2015-06-14 13:20:47 +0800

    first

---
+++ iNurse/iNurse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iNurse
+//
+//  Created by cyf on 15/6/14.
+//  Copyright © 2015年 com.dhccnurse.cyf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
